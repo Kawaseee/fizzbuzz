f57bdd93ecd6cf80e09c353cc863e4827c4cfb47
kawaseee <mktninsto1027@gmail.com>
2024-06-15 10:15:33 +0900

    first commit

---
+++ .venv/fizz.py
@@ -0,0 +1,12 @@
+def fb(n):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+
+i = 1
+while i <= 20:
+ print(i, fb(i))
+ i = i + 1
